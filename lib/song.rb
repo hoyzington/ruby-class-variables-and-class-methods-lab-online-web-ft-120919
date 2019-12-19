f6957322af4b1e005409c92d0c2363b5c8614d05
Scott Hoisington <hoyzington@gmail.com>
2019-12-19 01:33:27 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+  
+  @@title = 0
+  @@artist = 0
+  @@genre = 0
+  
+  def initialize(title, artist, genre)
+    @title = title
+    @artist = artist
+    @genre = genre
+    
+  
+end
