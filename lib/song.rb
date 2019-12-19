8597358ab7e6ca0f8218ca44b775174a2c8acf83
Scott Hoisington <hoyzington@gmail.com>
2019-12-19 01:54:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,31 @@ class Song
   
   attr_accessor :title, :artist, :genre
   
-  @@title = 0
-  @@artist = 0
-  @@genre = 0
+  @@titles = []
+  @@artists = []
+  @@genres = []
+  @@count = 0
   
   def initialize(title, artist, genre)
     @title = title
     @artist = artist
     @genre = genre
-    @@title += 1
-    @@artist += 1
-    @@genre += 1
+    @@titles << title
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.count
+    @@count
   end
   
 end
